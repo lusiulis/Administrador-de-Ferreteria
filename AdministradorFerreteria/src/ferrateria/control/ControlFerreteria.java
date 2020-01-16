6251ba8df93755dc23e1269907e027e8c219d0e9
Luis Vargas Benambourg <lusiulis@gmail.com>
2020-01-16 09:40:49 -0600

    Clases Principales

---
+++ AdministradorFerreteria/src/ferrateria/control/ControlFerreteria.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ferrateria.control;
+
+import ferreteria.model.ModeloFerreteria;
+
+/**
+ *
+ * @author lusiu
+ */
+public class ControlFerreteria {
+    
+    private final ModeloFerreteria modelo;
+
+    public ControlFerreteria() {
+        modelo = new ModeloFerreteria();
+    }
+    
+    
+}
