880d5e25ba15832ffe581de9bedcc39a95edc8c5
Luis Vargas Benambourg <lusiulis@gmail.com>
2020-01-16 10:59:51 -0600

    same

--- AdministradorFerreteria/src/administradorferreteria/AdministradorFerreteria.java
+++ AdministradorFerreteria/src/administradorferreteria/AdministradorFerreteria.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package administradorferreteria;
 
-/**
- *
- * @author lusiu
- */
+import ferrateria.control.ControlFerreteria;
+import ferreteria.views.Ventana;
+
 public class AdministradorFerreteria {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ControlFerreteria gestor = new ControlFerreteria();
+        Ventana ventana = new Ventana(gestor);
     }
     
 }
