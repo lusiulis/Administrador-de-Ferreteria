53444060b78aec0fd16abd43f55d29457a98522d
Kevin Fallas <kevin.fallas.b@gmail.com>
2020-01-22 18:30:50 -0600

    funcionalidad del hilo que mide promedio por minuto

--- AdministradorFerreteria/src/ferrateria/control/ControlFerreteria.java
+++ AdministradorFerreteria/src/ferrateria/control/ControlFerreteria.java
@@ -21,6 +21,7 @@ public class ControlFerreteria {
 
     public ControlFerreteria() {
         modelo = new ModeloFerreteria();
+        iniciarContadorTransacciones();
     }
 
     public void registrar(Observer observador) {
@@ -29,37 +30,45 @@ public class ControlFerreteria {
 
     public void cerrarAplicacion() {
         modelo.deleteObservers();
+        modelo.detenerContadorTransacciones();
         System.exit(0);
     }
 
     public void suprimir(Observer observador) {
         modelo.deleteObserver(observador);
         if (modelo.countObservers() == 0) {
+            modelo.detenerContadorTransacciones();
             System.exit(0);
         }
     }
 
     public void agregarProductoAFactura(Integer codigo, Integer cantidad) {
         modelo.agregarProducto(codigo, cantidad);
+        modelo.aumentarTransacciones();
     }
 
     public void cancelarFactura() {
         modelo.cancelarFactura();
+        modelo.aumentarTransacciones();
     }
 
     public void vender(String vendedor) {
         modelo.vender(vendedor);
+        modelo.aumentarTransacciones();
     }
 
     public void solicitarFacturaActual() {
         modelo.getFacturaActual();
+        modelo.aumentarTransacciones();
     }
 
     public void eliminarArticuloDeFactura(Integer numeroArt) {
         modelo.eliminarArticulo(numeroArt);
+        modelo.aumentarTransacciones();
     }
 
     public List<Producto> buscarArticulos(String nombreProducto, String tipoProducto) {
+        modelo.aumentarTransacciones();
         if (nombreProducto.equals("")) {
             if (!tipoProducto.equals("Ambos")) {
                 return ProductoDAO.getInstancia().ListarTipo(tipoProducto);
@@ -70,18 +79,26 @@ public class ControlFerreteria {
     }
 
     public Boolean eliminarDeInventario(Integer codigoArticulo) {
+        modelo.aumentarTransacciones();
         return ProductoDAO.getInstancia().Borrar(codigoArticulo);
     }
 
     public Boolean agregarAInventario(Producto productoNuevo) {
+        modelo.aumentarTransacciones();
         return ProductoDAO.getInstancia().AgregarProducto(productoNuevo);
     }
 
     public Boolean modificarEnInventario(Producto productoAModificar) {
+        modelo.aumentarTransacciones();
         return ProductoDAO.getInstancia().Modificar(productoAModificar);
     }
 
     public Producto getProducto(Integer codigoProducto) {
+        modelo.aumentarTransacciones();
         return ProductoDAO.getInstancia().recuperarProducto(codigoProducto);
+    }
+    
+    private void iniciarContadorTransacciones(){
+        modelo.iniciarContadorTransacciones();
     }
 }
--- AdministradorFerreteria/src/ferreteria/model/ModeloFerreteria.java
+++ AdministradorFerreteria/src/ferreteria/model/ModeloFerreteria.java
@@ -12,6 +12,8 @@ import ferreteria.model.entidades.Detalle;
 import ferreteria.model.entidades.Factura;
 import ferreteria.model.entidades.Producto;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -26,7 +28,7 @@ public class ModeloFerreteria extends Observable {
     private Factura facturaActual;
     private Integer cantTransacciones;//cantidad de transacciones realizadas 
     private ScheduledExecutorService exec;
-    
+
     public ModeloFerreteria() {
         facturaActual = new Factura();
         cantTransacciones = 0;
@@ -83,17 +85,36 @@ public class ModeloFerreteria extends Observable {
     }
 
     public void iniciarContadorTransacciones() {
-         exec = Executors.newSingleThreadScheduledExecutor();
+        exec = Executors.newSingleThreadScheduledExecutor();
         exec.scheduleAtFixedRate(new Runnable() {
+
+            Integer actualizaciones = 0;
+            List<Integer> transaccionesRealizadasCadaMinuto = new ArrayList();
+
             @Override
             public void run() {
+                Integer sumaTotalTransacciones = 0;
+                for (Integer i : transaccionesRealizadasCadaMinuto) {
+                    sumaTotalTransacciones += i;
+                }
                 setChanged();
-                notifyObservers("Promedio: "+cantTransacciones);
+                if (transaccionesRealizadasCadaMinuto.size() == 0) {
+                    notifyObservers("Promedio Transacciones/minuto: " + 0 + "                Transacciones este minuto: " + cantTransacciones + "                Minutos en ejecucion: " + 0);
+
+                } else {
+                    notifyObservers("Promedio Transacciones/minuto: " + sumaTotalTransacciones / transaccionesRealizadasCadaMinuto.size() + "                Transacciones este minuto: " + cantTransacciones + "                Minutos en ejecucion: " + transaccionesRealizadasCadaMinuto.size());
+                }
+                actualizaciones++;
+                if (actualizaciones == 120) {
+                    actualizaciones = 0;
+                    transaccionesRealizadasCadaMinuto.add(cantTransacciones);
+                    cantTransacciones = 0;
+                }
             }
         }, 0, 500, TimeUnit.MILLISECONDS);
     }
-    
-    public void detenerContadorTransacciones(){
+
+    public void detenerContadorTransacciones() {
         exec.shutdown();
     }
 }
