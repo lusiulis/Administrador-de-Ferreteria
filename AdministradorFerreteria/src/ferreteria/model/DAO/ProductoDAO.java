0cc37d2aeb1d40704789a5e8fc15a31cf6da06ec
Luis Vargas Benambourg <lusiulis@gmail.com>
2020-01-22 18:10:48 -0600

    DAO

--- AdministradorFerreteria/src/ferreteria/model/DAO/ProductoDAO.java
+++ AdministradorFerreteria/src/ferreteria/model/DAO/ProductoDAO.java
@@ -94,21 +94,6 @@ public class ProductoDAO {
             smt.setInt(8, nuevo.getId());
             
             Exito = smt.executeUpdate() == 1;
-        try(Connection cnx = gestor.obtenerConexion();){
-            Statement stm = cnx.createStatement();
-            Double longitud =0d;
-            if(nuevo.getLongitud()!=null){
-                longitud=nuevo.getLongitud();
-            }
-            String m =  "UPDATE producto Set Nombre = '" +nuevo.getNombre()
-                        +"', Cantidad = '" + nuevo.getCantidad()
-                        +"', Descripcion = '" + nuevo.getDescripcion()
-                        +"', Precio = '" + nuevo.getPrecio()
-                        +"', Tipo = '" + nuevo.getTipo()
-                        +"', Longitud ='" +longitud
-                        +"', CapacidadTrabajo = '" + nuevo.getCapacidadTrabajo() 
-                        + "' WHERE idProducto = " +nuevo.getId();
-            Exito = stm.executeUpdate(m) == 1;         
         } catch (SQLException ex) {
             System.err.printf("Excepción: '%s'%n", ex.getMessage());
         }
